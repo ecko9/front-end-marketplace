58adfb622ceaedb71045123046183c042ecc7edd
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-11-30 19:10:12 +0100

    merging issues

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from 'pages/Home';
 import Login from 'pages/Login';
 import RealEstateList from 'components/RealEstateList';
+import NavBar from 'components/NavBar';
 
 const App = () => {
   return (
     <div className='App'>
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
