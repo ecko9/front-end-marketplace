737c2d61d0b00420e5f77e8ea6f1ef4824f574ca
GXcode95 <xavier.grenouillet@gmail.com>
2021-11-30 22:52:16 +0100

    add ThemeProvider to add component + CssBaseline

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,29 @@ import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Profile from 'pages/Profile';
 import NavBar from 'components/NavBar';
-
+import { useSelector } from 'react-redux';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 const App = () => {
+  const theme = useSelector(state => state.themeReducer.theme)
   return (
     <div className='App'>
-      <Router>
-        <NavBar />
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        {/* Needed in order that css base properties like body background color  match the current theme 
+          if you want to disable it or overwrite it in a specific component use Paper from @mui/material/paper
+          Paper info => https://mui.com/components/paper/#main-content
+        */}
+        <CssBaseline /> 
+
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </Router>
+
+      </ThemeProvider>
     </div>
   );
 }
