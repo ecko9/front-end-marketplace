2f581c963d59e1b47e593070434ceaf1bfee17cd
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-01 00:52:03 +0100

    remove the LogginButton from page Home, button working, but not styled

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import RealEstateList from 'components/RealEstateList'
 import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import LoginButton from 'components/buttons/LoginButton';
        
 const Home = () => {
 
   return (
     <Box>
-      <LoginButton />
-        <Typography variant="h2" component="div"  sx={{ mr: 4 } }>
-          Bienvenue sur Immobills, venez vendre ou acheter vos biens!
-        </Typography>
+      <Typography variant="h2" component="div"  sx={{ mr: 4 } }>
+        Bienvenue sur Immobills, venez vendre ou acheter vos biens!
+      </Typography>
+      <RealEstateList />
     </Box>
   )
 }
