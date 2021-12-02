bbe797446d80c41e37407b105a911d6ce68f575f
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-02 17:03:30 +0100

    create MultipleFileUploadDropzone

---
+++ src/components/upload/MultipleFilesDropzone/FileDropzone.jsx
@@ -0,0 +1,64 @@
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Image } from 'cloudinary-react'
+import APIManager from 'services/Api'
+import { useSelector } from 'react-redux'
+import {cloudName, uploadPreset} from 'config/cloudinary.js'
+import {Stack} from '@mui/material'
+
+const MultipleFilesDropzone = () => {
+  const [uploadedFilesID, setUploadedFilesID] = useState()
+  const user = useSelector(state => state.userReducer.user) 
+  
+  const onDrop = useCallback((acceptedFiles) => {
+    
+    const url = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`
+    acceptedFiles.forEach(async (file) => {
+      const formData = new FormData()
+      formData.append("file", file)
+      formData.append("upload_preset",uploadPreset) // Needed to upload on cloudinary
+
+      const response = await fetch(url,{
+        method: 'post',
+        body: formData
+      })
+      const data = await response.json()
+      console.log("DATA", data)
+      setUploadedFilesID([...uploadedFilesID, data.public_id])
+    })
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    onDrop,
+    accepts: "image/*", /* */
+    multiple: true,
+  })
+    uploadedFilesID.forEach(fileID => {
+      APIManager.updateUserAvatar(fileID, user.id)
+    })
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+      <Stack direction="column">
+      {uploadedFilesID.map(fileID => (
+        <Image 
+          cloudName={cloudName}
+          publicId={fileID}
+          width="300"
+          crop="scale"
+          key={fileID}
+        />
+      ))}
+      </Stack>
+    </div>
+  )
+}
+
+export default MultipleFilesDropzone
