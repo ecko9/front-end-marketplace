3e939b6a85e61f80114344c24b4c5eae4fc1d734
ecko9 <jean.ferstler@gmail.com>
2021-12-02 18:15:18 +0100

    add avatar to profileinfo

--- src/components/ProfileInfo/index.jsx
+++ src/components/ProfileInfo/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import {Typography, Box } from '@mui/material';
+import { Typography, Box, Avatar } from '@mui/material';
 
-const ProfileInfo = ({profile}) => {
+const ProfileInfo = ({ profile }) => {
   return (
     <Box>
       <Typography variant="h2" color="primary" component="p">
         Profile
       </Typography>
+      <Stack direction="row" spacing={2}>
+        <Avatar sx={{ bgcolor: "blue" }}>{profile.username[0]}</Avatar>
+      </Stack>
       <Typography variant="h6" component="p">
         Username: {profile.username}
       </Typography>
       <Typography variant="h6" component="p">
         Email: {profile.email}
       </Typography>
-    </Box>
+    </Box >
   )
 }
-    
+
 export default ProfileInfo
